a2c42d55bc4e3d3ac35123e10c5ab82c65357878
Valentín <htmgarcia@gmail.com>
2014-10-23 19:40:01 -0500

    Change update/install messages

--- src/views/installer/tmpl/header_install.php
+++ src/views/installer/tmpl/header_install.php
@@ -1,3 +1,4 @@
 <?php include 'header_default.php'; ?>
-<h3>Thanks for installing <?php echo $name; ?>!</h3>
+<?php $name = explode(' - ', $name); ?>
+<h3>Thanks for installing <?php echo $name[1]; ?>!</h3>
 <div>Version: <?php echo $this->manifest->version; ?></div>
