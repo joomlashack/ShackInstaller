e39c2d5902a3d57a4dd2a2fa826e9c41640acd4f
Bill Tomczak <bill@joomlashack.com>
2021-10-19 06:53:14 -0700

    Release v2.1.1

--- src/library/Installer/AbstractScript.php
+++ src/library/Installer/AbstractScript.php
@@ -51,7 +51,7 @@ require_once 'include.php';
 
 abstract class AbstractScript
 {
-    public const VERSION = '2.1.0';
+    public const VERSION = '2.1.1';
 
     /**
      * @var bool
--- src/library/Installer/include.php
+++ src/library/Installer/include.php
@@ -35,6 +35,6 @@ if (class_exists('\\Alledia\\Installer\\AutoLoader') === false) {
 AutoLoader::register('Alledia\\Installer', SHACK_INSTALLER_BASE, true);
 
 if (!defined('SHACK_INSTALLER_VERSION')) {
-    define('SHACK_INSTALLER_VERSION', '2.1.0');
+    define('SHACK_INSTALLER_VERSION', '2.1.1');
     define('SHACK_INSTALLER_COMPATIBLE', '2.1.0');
 }
