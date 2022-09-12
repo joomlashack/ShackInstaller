38cd7f512e37ec4fce4ffe3d4e0bb8af4b7ce747
Bill Tomczak <bill@joomlashack.com>
2022-09-12 12:05:13 -0700

    Release v2.2.2

--- src/library/Installer/AbstractScript.php
+++ src/library/Installer/AbstractScript.php
@@ -52,7 +52,7 @@ require_once 'include.php';
 
 abstract class AbstractScript
 {
-    public const VERSION = '2.2.1';
+    public const VERSION = '2.2.2';
 
     /**
      * Recognized installation types
