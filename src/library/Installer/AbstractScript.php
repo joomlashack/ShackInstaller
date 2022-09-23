f2ba0d6a93d951c4a0708b312e332e3b6b5cd9ed
Bill Tomczak <bill@joomlashack.com>
2022-09-23 12:11:27 -0700

    Bump dev version

--- src/library/Installer/AbstractScript.php
+++ src/library/Installer/AbstractScript.php
@@ -52,7 +52,7 @@ require_once 'include.php';
 
 abstract class AbstractScript
 {
-    public const VERSION = '2.2.2';
+    public const VERSION = '2.2.3b1';
 
     /**
      * Recognized installation types
